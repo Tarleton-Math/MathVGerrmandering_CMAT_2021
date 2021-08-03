c38201cd0cdae3246dbc991c741690c03e05efc6
Tarleton-Math <scook@tarleton.edu>
2021-08-03 15:12:31 +0000

    commiting

--- src/Core.py
+++ src/Core.py
@@ -12,16 +12,24 @@ import warnings
 warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
 warnings.filterwarnings('ignore', message='.*Pyarrow could not determine the type of columns*')
 
+user_name = input('user_name (default=cook)')
+user_name = 'cook' if user_name=='' else user_name
+
+proj_id = input('proj_id (default=cmat-315920)')
+proj_id = 'cmat-315920' if proj_id=='' else proj_id
+
+random_seed = input('random_seed (default=42)')
+try:
+    rng = np.random.RandomState(random_seed)
+except:
+    rng = np.random.RandomState(42)
+    
 pd.set_option('display.max_columns', None)
 cred, proj = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
 bqclient   = bigquery.Client(credentials=cred, project=proj)
 root_path  = pathlib.Path(root_path)
 data_path  = root_path / 'redistricting_data'
 bq_dataset = proj_id   +'.redistricting_data'
-try:
-    rng = np.random.RandomState(seed)
-except:
-    rng = np.random.RandomState(42)
 
 Levels = ['tabblock', 'bg', 'tract', 'cnty', 'state', 'cntyvtd']
 District_types = ['cd', 'sldu', 'sldl']
