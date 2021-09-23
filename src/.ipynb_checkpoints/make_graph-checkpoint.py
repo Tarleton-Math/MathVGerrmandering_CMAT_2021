e8408d53f8be52a58139a61bbee618fb03ef0cdc
drscook <scook@tarleton.edu>
2021-09-23 00:01:19 +0000

    commiting

--- src/.ipynb_checkpoints/make_graph-checkpoint.py
+++
@@ -1,6 +0,0 @@
-proj_id = 'cmat-315920'
-
-import pandas as pd, geopandas as gpd, networkx as nx
-
-cred, proj = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
-bqclient   = google.cloud.bigquery.Client(credentials=cred, project=proj)
