bdc6094e33fe7c90e79dab5990facf526a8cb55a
drscook <scook@tarleton.edu>
2021-11-02 23:48:49 +0000

    commiting

--- redistricter.py
+++ redistricter.py
@@ -50,7 +50,6 @@ for opt, val in {**opts, **run_opts}.items():
     print(f'{opt.ljust(22, " ")}: {val}')
     
 task = input('Using options above - do you want to (r)un MCMC, (p)ost-process each run, (c)onsolidate results, or any other to quit: ').lower()
-task = 'r'
 
 a = run_opts['seed_start']
 b = min(a + run_opts['jobs_per_worker'] * run_opts['workers'], run_opts['seed_stop'])
@@ -61,6 +60,7 @@ if task in ['r', 'run', 'p']:
         print(f'starting seed {random_seed}')
         M = MCMC(random_seed=random_seed, **opts)
         if task == 'p':
+            print('post-processing')
             M.post_process()
         else:
             M.run_chain()
